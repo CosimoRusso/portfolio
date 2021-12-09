87aa885a019c78477526d04b44ab27dc3c8edf12
Cosimo Russo <cosimo.russo123@gmail.com>
2021-12-09 13:34:42 -0300

    Fix graphic and text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ function Projects(){
             <h1>My Projects</h1>
             <p>I started working in my first startup when I was 17. Now I am {new Date().getFullYear() - 1997},
                 and I have worked in many projects. Most of them were for companies, some were a success, others failed,
-                some are not publicly available. In this section you can find a small selection of the ones I can show.</p>
+                some are not publicly available. In this section you can find a small selection of them.</p>
             {projects.map(p =>
                 <ProjectBox {...p} />
             )}
