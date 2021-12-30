fd1ef1baca9eb95ff89f4083adfcfe5b1e512e07
Cosimo Russo <cosimo.russo123@gmail.com>
2021-12-30 14:29:17 +0100

    Update intro

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -3,8 +3,8 @@ function Presentation(){
         <div className={'presentation'}>
             <h1>Hello, my name is Cosimo Russo</h1>
             <p>
-                I'm a full-stack developer and about to graduate as a master student in
-                Computer Science and Engineering in Politecnico di Milano. During my
+                I'm a junior software engineer graduated as a master student in
+                Computer Science and Engineering in Politecnico di Milano, with final mark of 110/110 cum laude. During my
                 studies I have worked on various projects in different startups, some of which you can see in this page.
             </p>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ function Projects(){
         <Container maxWidth={'lg'}>
             <h1>My Projects</h1>
             <p>I started working in my first startup when I was 17. Now I am {new Date().getFullYear() - 1997},
-                and I have worked in many projects. Most of them were for companies, some were a success, others failed,
+                and I have worked on many projects. Most of them were for companies, some were a success, others failed,
                 some are not publicly available. In this section you can find a small selection of them.</p>
             {projects.map(p =>
                 <ProjectBox {...p} />
